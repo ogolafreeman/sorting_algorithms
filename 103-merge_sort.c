5c5dbe6f1e67f88bb6c1416080606a9768b05bc9
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-07-27 00:10:44 -0700

    7. Merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,7 @@
 #include "sort.h"
 
-void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back);
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
 void merge_sort(int *array, size_t size);
 
@@ -12,7 +13,8 @@ void merge_sort(int *array, size_t size);
  * @mid: ...
  * @back: ...
  */
-void merge_subarr(int *subarr, int *buff, size_t front, size_t mid, size_t back)
+void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
+		size_t back)
 {
 	size_t i, j, k = 0;
 
